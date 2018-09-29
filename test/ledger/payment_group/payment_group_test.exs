d5cedc217ab833eaa36af79031984d05999d355c
Bradley Stachurski <bradley.stachurski@gmail.com>
2018-09-29 13:33:00 -0600

    Fix failing tests in PaymentGroup

--- test/ledger/payment_group/payment_group_test.exs
+++ test/ledger/payment_group/payment_group_test.exs
@@ -2,10 +2,9 @@ defmodule Ledger.PaymentGroupTest do
   use Ledger.DataCase
 
   alias Ledger.PaymentGroup
+  alias Ledger.PaymentGroup.{Group, Participant}
 
   describe "groups" do
-    alias Ledger.PaymentGroup.Group
-
     @valid_attrs %{name: "some name"}
     @update_attrs %{name: "some updated name"}
     @invalid_attrs %{name: nil}
@@ -63,65 +62,70 @@ defmodule Ledger.PaymentGroupTest do
     end
   end
 
-#  describe "participants" do
-#    alias Ledger.PaymentGroup.Participant
-#
-#    @valid_attrs %{amount: 42, name: "some name"}
-#    @update_attrs %{amount: 43, name: "some updated name"}
-#    @invalid_attrs %{amount: nil, name: nil}
-#
-#    def participant_fixture(attrs \\ %{}) do
-#      {:ok, participant} =
-#        attrs
-#        |> Enum.into(@valid_attrs)
-#        |> PaymentGroup.create_participant()
-#
-#      participant
-#    end
-#
-#    test "list_participants/0 returns all participants" do
-#      participant = participant_fixture()
-#      assert PaymentGroup.list_participants() == [participant]
-#    end
-#
-#    test "get_participant!/1 returns the participant with given id" do
-#      participant = participant_fixture()
-#      assert PaymentGroup.get_participant!(participant.id) == participant
-#    end
-#
-#    test "create_participant/1 with valid data creates a participant" do
-#      assert {:ok, %Participant{} = participant} = PaymentGroup.create_participant(@valid_attrs)
-#      assert participant.amount == 42
-#      assert participant.name == "some name"
-#    end
-#
-#    test "create_participant/1 with invalid data returns error changeset" do
-#      assert {:error, %Ecto.Changeset{}} = PaymentGroup.create_participant(@invalid_attrs)
-#    end
-#
-#    test "update_participant/2 with valid data updates the participant" do
-#      participant = participant_fixture()
-#      assert {:ok, participant} = PaymentGroup.update_participant(participant, @update_attrs)
-#      assert %Participant{} = participant
-#      assert participant.amount == 43
-#      assert participant.name == "some updated name"
-#    end
-#
-#    test "update_participant/2 with invalid data returns error changeset" do
-#      participant = participant_fixture()
-#      assert {:error, %Ecto.Changeset{}} = PaymentGroup.update_participant(participant, @invalid_attrs)
-#      assert participant == PaymentGroup.get_participant!(participant.id)
-#    end
-#
-#    test "delete_participant/1 deletes the participant" do
-#      participant = participant_fixture()
-#      assert {:ok, %Participant{}} = PaymentGroup.delete_participant(participant)
-#      assert_raise Ecto.NoResultsError, fn -> PaymentGroup.get_participant!(participant.id) end
-#    end
-#
-#    test "change_participant/1 returns a participant changeset" do
-#      participant = participant_fixture()
-#      assert %Ecto.Changeset{} = PaymentGroup.change_participant(participant)
-#    end
-#  end
+  describe "participants" do
+    @valid_attrs %{amount: 42, name: "some name"}
+    @update_attrs %{amount: 43, name: "some updated name"}
+    @invalid_attrs %{group_id: nil, amount: nil, name: nil}
+
+    def participant_fixture(attrs \\ %{}) do
+      %Group{id: group_id} = group_fixture()
+      {:ok, participant} =
+        attrs
+        |> Enum.into(%{group_id: group_id})
+        |> Enum.into(@valid_attrs)
+        |> PaymentGroup.create_participant()
+
+      {participant, group_id}
+    end
+
+    test "list_participants/1 returns all participants" do
+      {participant, group_id} = participant_fixture()
+      assert PaymentGroup.list_participants(group_id) == [participant]
+    end
+
+    test "get_participant!/1 returns the participant with given id" do
+      {participant, _group_id} = participant_fixture()
+      assert PaymentGroup.get_participant!(participant.id) == participant
+    end
+
+    test "create_participant/1 with valid data creates a participant" do
+      %Group{id: group_id} = group_fixture()
+      assert {:ok, %Participant{} = participant} =
+        %{group_id: group_id}
+        |> Enum.into(@valid_attrs)
+        |> PaymentGroup.create_participant()
+
+      assert participant.amount == 42
+      assert participant.name == "some name"
+    end
+
+    test "create_participant/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = PaymentGroup.create_participant(@invalid_attrs)
+    end
+
+    test "update_participant/2 with valid data updates the participant" do
+      {participant, _group_id} = participant_fixture()
+      assert {:ok, participant} = PaymentGroup.update_participant(participant, @update_attrs)
+      assert %Participant{} = participant
+      assert participant.amount == 43
+      assert participant.name == "some updated name"
+    end
+
+    test "update_participant/2 with invalid data returns error changeset" do
+      {participant, _group_id} = participant_fixture()
+      assert {:error, %Ecto.Changeset{}} = PaymentGroup.update_participant(participant, @invalid_attrs)
+      assert participant ==  PaymentGroup.get_participant!(participant.id)
+    end
+
+    test "delete_participant/1 deletes the participant" do
+      {participant, _group_id} = participant_fixture()
+      assert {:ok, %Participant{}} = PaymentGroup.delete_participant(participant)
+      assert_raise Ecto.NoResultsError, fn -> PaymentGroup.get_participant!(participant.id) end
+    end
+
+    test "change_participant/1 returns a participant changeset" do
+      {participant, _group_id} = participant_fixture()
+      assert %Ecto.Changeset{} = PaymentGroup.change_participant(participant)
+    end
+  end
 end
