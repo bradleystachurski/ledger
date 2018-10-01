88b00a1904a4a98a5b9a1661da41078bca3613a5
Bradley Stachurski <bradley.stachurski@gmail.com>
2018-10-01 02:17:06 -0600

    Add script to seed database

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,57 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Ledger.Repo
+alias Ledger.PaymentGroup
+alias Ledger.PaymentGroup.{Group, Participant}
+
+{:ok, %Group{id: ef_group_id}} =
+  %{name: "Ethereum Foundation"}
+  |> PaymentGroup.create_group()
+
+{:ok, %Group{id: csys_group_id}} =
+  %{name: "ConsenSys"}
+  |> PaymentGroup.create_group()
+
+Repo.insert! %Participant{
+  username: "Vitalik",
+  group_id: ef_group_id,
+  amount: 100
+}
+
+Repo.insert! %Participant{
+  username: "Vlad",
+  group_id: ef_group_id,
+  amount: 100
+}
+
+Repo.insert! %Participant{
+  username: "Gavin",
+  group_id: ef_group_id,
+  amount: 100
+}
+
+Repo.insert! %Participant{
+  username: "Danny",
+  group_id: ef_group_id,
+  amount: 100
+}
+
+Repo.insert! %Participant{
+  username: "Joe",
+  group_id: csys_group_id,
+  amount: 100
+}
+
+Repo.insert! %Participant{
+  username: "Dudley",
+  group_id: csys_group_id,
+  amount: 100
+}
+
+Repo.insert! %Participant{
+  username: "Kevin",
+  group_id: csys_group_id,
+  amount: 100
+}
